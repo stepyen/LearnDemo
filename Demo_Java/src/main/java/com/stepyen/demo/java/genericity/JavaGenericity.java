ec95a5a07b02eb849049bff79b14f30b2c984e09
stepyen <stepyen@163.com>
2021-01-06 09:10:28 +0800

    feat:java 泛型

---
+++ Demo_Java/src/main/java/com/stepyen/demo/java/genericity/JavaGenericity.java
@@ -0,0 +1,30 @@
+package com.stepyen.demo.java.genericity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * date：2019/9/6
+ * author：stepyen
+ * description：Java 泛型学习
+ */
+public class JavaGenericity {
+    public static class Animal{
+
+    }
+
+    public static class Dog extends Animal {
+
+    }
+
+
+    public static void main(String[] args) {
+        List<Dog> dogList = new ArrayList<Dog>();
+        List<? extends Animal> animalList = dogList;
+
+
+    }
+
+
+
+}
