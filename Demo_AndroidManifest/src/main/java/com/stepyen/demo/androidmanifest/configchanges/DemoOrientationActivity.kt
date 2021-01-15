52beb8c6fb9ccf4553a2ef47f30358532caddbab
stepyen <stepyen@163.com>
2021-01-15 15:11:40 +0800

    feat:onConfigurationChanged

--- Demo_AndroidManifest/src/main/java/com/stepyen/demo/androidmanifest/configchanges/DemoOrientationActivity.kt
+++ Demo_AndroidManifest/src/main/java/com/stepyen/demo/androidmanifest/configchanges/DemoOrientationActivity.kt
@@ -1,8 +1,11 @@
 package com.stepyen.demo.androidmanifest.configchanges
 
 import android.content.res.Configuration
+import android.view.ViewGroup
+import com.stepyen.demo.androidmanifest.R
 import com.stepyen.demo.base.base.BasePageActivity
 import com.stepyen.demo.base.utils.L
+import kotlinx.android.synthetic.main.activity_orientation.*
 
 /**
  * date：2021/1/11
@@ -15,6 +18,7 @@ class DemoOrientationActivity : BasePageActivity(){
     override var TAG: String = "DemoOrientationActivity_TAG"
 
     override fun initView() {
+        addView(R.layout.activity_orientation)
     }
 
 
@@ -22,5 +26,6 @@ class DemoOrientationActivity : BasePageActivity(){
         super.onConfigurationChanged(newConfig)
         L.d(TAG,newConfig?.toString())
 
+        iv.setImageResource(R.drawable.ic_mountain)
     }
 }
