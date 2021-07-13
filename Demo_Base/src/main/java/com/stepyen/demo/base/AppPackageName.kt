e525467df624ca482fb62451d2b0a51fc3258127
stepyen <stepyen@163.com>
2021-07-13 16:44:15 +0800

    fix:第三方app包名

---
+++ Demo_Base/src/main/java/com/stepyen/demo/base/AppPackageName.kt
@@ -0,0 +1,29 @@
+package com.stepyen.demo.base
+
+/**
+ * date：7/13/21
+ * author：stepyen
+ * description：第三方app包名
+ *
+ */
+class AppPackageName {
+
+
+    companion object {
+
+        const val T3 = "com.t3go.passenger"// T3
+
+        const val TAOBAO = "com.taobao.taobao"  // 淘宝
+
+        const val KUAISHOU_FAST = "com.kuaishou.nebula" // 快手极速版
+
+        const val UC_BROWSER = "com.UCMobile" // uc浏览器
+
+        const val WEIBO = "com.sina.weibo" // 微博
+
+        const val BAIDU_BROWSER = "com.baidu.searchbox" // 百度浏览器
+
+
+    }
+}
+
