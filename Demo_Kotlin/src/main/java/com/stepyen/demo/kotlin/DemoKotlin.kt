771d7bbb9bf39511f98e74a978893bf7346b1630
stepyen <stepyen@163.com>
2021-12-01 09:58:11 +0800

    1

---
+++ Demo_Kotlin/src/main/java/com/stepyen/demo/kotlin/DemoKotlin.kt
@@ -0,0 +1,17 @@
+package com.stepyen.demo.kotlin
+
+/**
+ * date：2021/12/1
+ * author：stepyen
+ * description：
+ *
+ */
+object DemoKotlin {
+
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+
+    }
+
+}
